c28ce5a3210c5b971da285787e0560e0da4ff3b1
unknown <ernestchiu@hotmail.ca>
2019-07-07 11:32:49 -0400

    added initial seeding for dashboard values

--- money_manager/src/components/Dashboard.js
+++ money_manager/src/components/Dashboard.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
+import Table from "react-bootstrap/Table";
 
 class Dashboard extends Component {
   render() {
-    return <div className="Dashboard">Welcome to the Dashboard page</div>;
+    return (
+      <div className="Dashboard">
+        <Table variant="dark" responsive bordered hover>
+          <thead>
+            <tr>
+              <th>Catagory</th>
+              <th>Cost</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>food</td>
+              <td>10.50</td>
+              <td>2019-07-02</td>
+            </tr>
+          </tbody>
+
+          <tbody>
+            <tr>
+              <td>groceries</td>
+              <td>40.00</td>
+              <td>2019-06-23</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>gas</td>
+              <td>63.28</td>
+              <td>2019-06-28</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
   }
 }
 
