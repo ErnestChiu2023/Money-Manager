addd010286306429f5a3fe99d074edd29bab4438
unknown <ernestchiu@hotmail.ca>
2019-07-07 17:27:11 -0400

    edited get request to return all records

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json());
 app.use("/expense", expense);
 app.use("/income", income);
 
+let expenseModel = require("./models/expenses");
+let incomeModel = require("./models/incomes");
+
 mongoose.connect("mongodb://localhost/moneyManager");
 mongoose.connection
   .once("open", function() {
@@ -19,10 +22,27 @@ mongoose.connection
     console.log("Connectin error:", error);
   });
 
-app.get("/", function(req, res) {
-  res.send("hello world");
+app.get("/records", function(req, res) {
+  let count = 0;
+  var Records = {
+    expenses: null,
+    incomes: null
+  };
+  function callback() {
+    if (++count == 2) {
+      res.json(Records);
+    }
+  }
+  expenseModel.find({}).then(function(data) {
+    Records.expenses = data;
+    callback();
+  });
+  incomeModel.find({}).then(function(data) {
+    Records.incomes = data;
+    callback();
+  });
 });
 
-app.listen(3000, function() {
-  console.log("Now listening on port 3000");
+app.listen(4000, function() {
+  console.log("Now listening on port 4000");
 });
