8aca4b027961fd1d3e03c91f7d03604bac541985
unknown <ernestchiu@hotmail.ca>
2019-07-08 22:26:23 -0400

    added axios.js request to receive records from database

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const PORT = 80;
+
+app.use(bodyParser.json());
 
 var expense = require("./expense");
 var income = require("./income");
-
-app.use(bodyParser.json());
-app.use("/expense", expense);
-app.use("/income", income);
-
 let expenseModel = require("./models/expenses");
 let incomeModel = require("./models/incomes");
 
-mongoose.connect("mongodb://localhost/moneyManager");
+app.use("/expense", expense);
+app.use("/income", income);
+
+mongoose.connect("mongodb://localhost/moneyManager", { useNewUrlParser: true });
 mongoose.connection
   .once("open", function() {
     console.log("successfully connected to the database");
@@ -22,7 +26,7 @@ mongoose.connection
     console.log("Connectin error:", error);
   });
 
-app.get("/records", function(req, res) {
+app.get("/records", cors(), function(req, res) {
   let count = 0;
   var Records = {
     expenses: null,
@@ -43,6 +47,6 @@ app.get("/records", function(req, res) {
   });
 });
 
-app.listen(4000, function() {
+app.listen(PORT, function() {
   console.log("Now listening on port 4000");
 });
--- money_manager/src/components/Dashboard.js
+++ money_manager/src/components/Dashboard.js
@@ -1,41 +1,108 @@
 import React, { Component } from "react";
 import Table from "react-bootstrap/Table";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Container from "react-bootstrap/Container";
+const axios = require("axios");
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    };
+  }
+
+  state = {
+    expenses: [],
+    incomes: []
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:80/records/").then(res => {
+      this.setState({
+        expenses: res.data.expenses,
+        incomes: res.data.incomes
+      });
+    });
+  }
+
   render() {
     return (
       <div className="Dashboard">
-        <Table variant="dark" responsive bordered hover>
-          <thead>
-            <tr>
-              <th>Catagory</th>
-              <th>Cost</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>food</td>
-              <td>10.50</td>
-              <td>2019-07-02</td>
-            </tr>
-          </tbody>
+        <Container>
+          <Tabs defaultActiveKey="overview">
+            <Tab eventKey="overview" title="Overview">
+              Overview
+            </Tab>
+            <Tab eventKey="income" title="Income">
+              <Table variant="dark" responsive bordered hover>
+                <thead>
+                  <tr>
+                    <th>Catagory</th>
+                    <th>Cost</th>
+                    <th>Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>food</td>
+                    <td>10.50</td>
+                    <td>2019-07-02</td>
+                  </tr>
+                </tbody>
 
-          <tbody>
-            <tr>
-              <td>groceries</td>
-              <td>40.00</td>
-              <td>2019-06-23</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>gas</td>
-              <td>63.28</td>
-              <td>2019-06-28</td>
-            </tr>
-          </tbody>
-        </Table>
+                <tbody>
+                  <tr>
+                    <td>groceries</td>
+                    <td>40.00</td>
+                    <td>2019-06-23</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>gas</td>
+                    <td>63.28</td>
+                    <td>2019-06-28</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Tab>
+            <Tab eventKey="expenses" title="Expenses">
+              <Table variant="dark" responsive bordered hover>
+                <thead>
+                  <tr>
+                    <th>Catagory</th>
+                    <th>Cost</th>
+                    <th>Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>food</td>
+                    <td>10.50</td>
+                    <td>2019-07-02</td>
+                  </tr>
+                </tbody>
+
+                <tbody>
+                  <tr>
+                    <td>groceries</td>
+                    <td>40.00</td>
+                    <td>2019-06-23</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>gas</td>
+                    <td>63.28</td>
+                    <td>2019-06-28</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Tab>
+          </Tabs>
+        </Container>
       </div>
     );
   }
